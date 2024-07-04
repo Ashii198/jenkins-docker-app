31c92c00a86e127e8371d0db3f2a102384114ddf
rajaniekunde <151899905+rajaniekunde@users.noreply.github.com>
2024-07-04 15:02:05 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,64 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_security_group" "deployment_sg" {
+  name        = "deployment_sg"
+  description = "Security group for deployment server"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "deployment" {
+  ami           = "ami-0c55b159cbfafe1f0" # Ubuntu Server 20.04 LTS AMI ID for us-east-1 (N. Virginia)
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.deployment_sg.name]
+
+  tags = {
+    Name = "deployment"
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 
